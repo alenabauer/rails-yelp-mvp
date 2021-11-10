818e3e1ffd236dd43d4202e1751cbd854814fb40
Alena Bauer <alena.a.bauer@gmail.com>
2021-11-10 11:41:44 +0100

    add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+CATEGORIES = %w[chinese italian japanese french belgian];
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
